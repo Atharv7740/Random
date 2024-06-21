6732162b610aef0f113b5837617d37faba2c2ae3
Atharv7740 <atharv7740@gmail.com>
2024-06-22 00:04:18 +0530

    Added all essential files

---
+++ src/JavaExceptionHandling/JavaRuntimeException.java
@@ -0,0 +1,10 @@
+package JavaExceptionHandling;
+
+public class JavaRuntimeException {
+    public static void main(String[] args) {
+        int[] a =new int[4];
+        System.out.println(a[8]);
+
+    }
+
+}
