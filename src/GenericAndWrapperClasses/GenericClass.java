09b0b586a5acd82f35266950988280778b515d39
Atharv7740 <atharv7740@gmail.com>
2024-06-22 20:10:31 +0530

    Update in generic class

--- src/GenericAndWrapperClasses/GenericClass.java
+++ src/GenericAndWrapperClasses/GenericClass.java
@@ -5,6 +5,7 @@ public class GenericClass {
 // Dog<int> d=new Dog<>(15) only non premitive data type supported.
         Dog<String> d1= new Dog<>("1df2g");
         Dog<Integer> d2 =new Dog<>(143334);
+        Cat<Integer> c1 =new Cat<>(12342);
 
     }
 }
@@ -12,5 +13,11 @@ class Dog<E>{
     E id;
     public Dog(E id){
         this.id=id;
+    }
+}
+class Cat<C>{
+    C id;
+    public Cat(C id){
+        this.id =id;
     }
 }
